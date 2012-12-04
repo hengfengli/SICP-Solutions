027cfac314a532a75d893f0fe616d3ca5763f64f
HengfengLi <hengfeng12345@gmail.com>
2012-12-04 16:44:40 +1100

    e59-e60

---
+++ ch02/e60.rkt
@@ -0,0 +1,25 @@
+#lang scheme
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (union-set set1 set2)
+  (append set1 set2))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define x1 (list 2 3 2 1 3 2 2))
+(define x2 (list 1 2 5 2 2))
+
+(intersection-set x1 x2)
+(union-set x1 x2)
