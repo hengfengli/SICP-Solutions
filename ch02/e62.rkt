c86182890e92b5cc30ccd2221061fff0ef22e2ff
Hengfeng Li <hengfeng12345@gmail.com>
2012-12-04 23:32:59 +1100

    61-62

---
+++ ch02/e62.rkt
@@ -0,0 +1,42 @@
+#lang scheme
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (union-set set1 set2)
+    (cond ((null? set1) set2)
+          ((null? set2) set1)
+          ((= (car set1) (car set2))
+           (cons (car set1) (union-set (cdr set1) (cdr set2))))
+          ((< (car set1) (car set2))
+           (cons (car set1) (union-set (cdr set1) set2)))
+          (else
+           (cons (car set2) (union-set set1 (cdr set2))))))
+
+(define t1 (list 1 2 5 6 9))
+(define t2 (list 1 3 4 6 10))
+
+(intersection-set t1 t2)
+(union-set t1 t2)
